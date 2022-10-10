04ecbc3d992782e1b319d9d3e4bc38db769adec4
Gurpreet <gs109690@gmail.com>
2022-10-10 16:14:09 -0500

    deploy scripts for contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+
+const GEORLI_RPC_URL = process.env.GEORLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API = process.env.ETHERSCAN_API
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+    networks: {
+        georli: {
+            url: process.env.GEORLI_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API,
+    },
+}

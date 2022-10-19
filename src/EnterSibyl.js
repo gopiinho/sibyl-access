e743ffdca11506707e8e7c659a368136bb58a1e7
Gurpreet <gs109690@gmail.com>
2022-10-18 22:53:18 -0500

    wagmi

--- src/EnterSibyl.js
+++ src/EnterSibyl.js
@@ -2,11 +2,32 @@ import React from "react"
 import { useState } from "react"
 import "./App.css"
 import NavBar from "./NavBar"
+import { useNetwork, useSwitchNetwork } from "wagmi"
 
 export function EnterSibyl() {
     const [accounts, setAccounts] = useState([])
+    const { chain } = useNetwork()
+    const { chains, error, isLoading, pendingChainId, switchNetwork } =
+      useSwitchNetwork()
 
     return (
+        <div>
+      {chain && <div>Connected to {chain.name}</div>}
+
+      {chains.map((x) => (
+        <button
+          disabled={!switchNetwork || x.id === chain?.id}
+          key={x.id}
+          onClick={() => switchNetwork?.(x.id)}
+        >
+          {x.name}
+          {isLoading && pendingChainId === x.id && ' (switching)'}
+        </button>
+      ))}
+
+      <div>{error && error.message}</div>
+    </div>
+
         <div className="overlay">
             <div className="App2">
                 <NavBar accounts={accounts} setAccounts={setAccounts} />
